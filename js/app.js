576599764e4bcf5c68317ad3d22dc8af8f3d18e7
Ben Tyler <benandrewtyler@gmail.com>
2018-12-19 13:27:00 +0000

    First commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -30,7 +29,7 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+sdfsfsafsdfs
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
